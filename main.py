ca756ae4140ca44c07ba4dbe5d0fe3e57aefaefc
giulia.dascenzi <giulia.dash@gmail.com>
2021-08-31 17:34:46 +0200

    minor fixes

--- main.py
+++ main.py
@@ -618,7 +618,7 @@ def print_table_Quadratic_SVM_minDCF(DTR, LTR, prior, cost_fn, cost_fp, k): #TOD
 
         #--------------- GAUSSIANIZED FEATURES-------------------------
         print("*** minDCF - GAUSSIANIZED FEATURES  ***")
-        quadratic_SVM_minDCF(gaussianizedFeatures C=C, c=c, K=K)
+        quadratic_SVM_minDCF(gaussianizedFeatures ,C=C, c=c, K=K)
 
 
         print("************************************************")
