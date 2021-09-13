00dd2f1ec7bd6368ebf02ad97afd562e0dc45965
patrizio de girolamo s248720 <s248720@studenti.polito.it>
2021-09-13 15:41:15 +0200

    prova comparison

--- evaluation_results.py
+++ evaluation_results.py
@@ -423,6 +423,63 @@ def print_table_GMM_minDCF(DTR, LTR, k, eval_data):
     bar_plot_gmm(raw_minDCFs, gau_minDCFs, gmm_comp, "GMM_Tied_Diagonal_covariance")
 
 
+
+def print_table_comparison_DCFs(DTR, LTR, k, data_eval):
+
+    def actDCF_minDCF(data, llr_calculator, params, data_eval):
+            prior=0.5
+            cost_fn=1
+            cost_fp=1
+            min_DCF_LR, act_DCF_LR,_ = model_validation.k_cross_DCF(data, LTR, k, llr_calculator, prior , cost_fn, cost_fp, params, data_eval)
+            print("[5-Folds]  -  prior= 0.5  minDCF: ",min_DCF_LR, " actDCF= ",act_DCF_LR)
+
+            prior=0.1
+            cost_fn=1
+            cost_fp=1
+            min_DCF_LR, act_DCF_LR,_ = model_validation.k_cross_DCF(data, LTR, k, llr_calculator, prior , cost_fn, cost_fp, params, data_eval)
+            print("[5-Folds]  -  prior= 0.1  minDCF: ",min_DCF_LR, " actDCF= ",act_DCF_LR) 
+
+            prior=0.9
+            cost_fn=1
+            cost_fp=1
+            min_DCF_LR, act_DCF_LR,_ = model_validation.k_cross_DCF(data, LTR, k, llr_calculator, prior , cost_fn, cost_fp, params, data_eval)
+            print("[5-Folds]  -  prior= 0.9  minDCF: ",min_DCF_LR, " actDCF= ",act_DCF_LR) 
+
+            print()
+
+
+    #------------------------FIRST MODEL ----------------- 
+
+    print(" RBF SVM, C=1, lam=1, pi_T =0.5 raw features ")
+    lam = 1
+    pi_T = 0.5
+    C= 1
+    actDCF_minDCF(preprocessing.Z_normalization(DTR), SVMClassifier.RBF_SVM_computeLogLikelihoods,[pi_T, C, lam], data_eval )
+
+    #--------------- SECOND MODEL-------------------------
+
+    print(" Quad SVM, C=0.1, pi_T =0.5, c=1,K=0 raw features  ")
+    C=0.1
+    pi_T=0.5
+    c=1
+    K=0
+    actDCF_minDCF(preprocessing.Z_normalization(DTR), SVMClassifier.Polinomial_SVM_computeLogLikelihoods,[pi_T,C,c,K, data_eval])
+
+#--------------------------
+def print_err_bayes_plots(data, L, k, llr_calculators, other_params, titles, colors):
+    plt.figure()
+    plt.title("Bayes Error Plot")
+    plt.xlabel("prior log odds")
+    plt.ylabel("DCF")
+    for i in range (len(llr_calculators)):
+        print("Working on calculator "+ str(i))
+        model_validation.bayes_error_plot(data[i], L, k, llr_calculators[i], other_params[i], titles[i], colors[i] )
+        print("DONE")
+    plt.savefig('Graph/Error_Bayes_Plots/EBP1.png' )
+
+
+
+'''
 def print_table_comparison_DCFs(DTR, LTR, k, eval_data):
 
     def actDCF_minDCF(data, llr_calculator, params, eval_data):
@@ -477,6 +534,7 @@ def print_err_bayes_plots(data, L, k, llr_calculators, other_params, titles, col
         print("DONE")
     plt.savefig('Graph/Error_Bayes_Plots/EBP1_EVAL.png' )
 
+'''
 #--------------------------
 def print_all(DTR, LTR, DEV, LEV, k):
     
@@ -548,7 +606,7 @@ def print_all(DTR, LTR, DEV, LEV, k):
     print("******************************************************************")
 
     #error bayes plot
-
+    '''
     pi_T1 = 0.5
     C1= 1
     lam1 = 1
@@ -570,7 +628,7 @@ def print_all(DTR, LTR, DEV, LEV, k):
     print_err_bayes_plots(data, LTR, k, llr_calculators, other_params, titles, colors, eval_data)
     print("*****************************************************")
     
-
+    '''
 
 
 
